608266bd98398720f57e949ec0d4470b0edce850
Manuel Stör <m.stoer@microstream.one>
2026-01-16 09:28:22 +0100

    Remove startup data generation (#2)
    Signed-off-by: Manuel Stör <m.stoer@microstream.one>

--- src/main/java/one/microstream/demo/dto/GenerateData.java
+++ src/main/java/one/microstream/demo/dto/GenerateData.java
@@ -2,45 +2,20 @@ package one.microstream.demo.dto;
 
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.core.annotation.NonNull;
-import io.micronaut.core.annotation.Nullable;
 import io.micronaut.serde.annotation.Serdeable;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Positive;
-import one.microstream.demo.bean.DataGenerationConfig;
-
-import java.util.Optional;
 
 @Introspected
 @Serdeable
 public record GenerateData(
-    @NonNull @Valid DataGenerationConfigImpl genreConf,
-    @NonNull @Valid DataGenerationConfigImpl authorConf,
-    @NonNull @Valid DataGenerationConfigImpl bookConf
+    @NonNull @Valid DataGenerationConfig genreConf,
+    @NonNull @Valid DataGenerationConfig authorConf,
+    @NonNull @Valid DataGenerationConfig bookConf
 )
 {
     @Introspected
     @Serdeable
-    public static class DataGenerationConfigImpl implements DataGenerationConfig
+    public record DataGenerationConfig(int count, Long seed)
     {
-        private final int count;
-        private final Long seed;
-
-        public DataGenerationConfigImpl(@Positive  int count, @Nullable Long seed)
-        {
-            this.count = count;
-            this.seed = seed;
-        }
-
-        @Override
-        public int getCount()
-        {
-            return count;
-        }
-
-        @Override
-        public Optional<Long> getSeed()
-        {
-            return Optional.ofNullable(seed);
-        }
     }
 }
